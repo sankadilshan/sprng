c9d84e5d0e68b22801b17a4143e2bf23d887e93d
sanka9686 <sankadilshan9686@gmail.com>
2019-08-23 05:32:59 +0530

    first commit

--- sprng/sprng/src/main/java/com/sprng/sprng/SprngApplication.java
+++ sprng/sprng/src/main/java/com/sprng/sprng/SprngApplication.java
@@ -1,14 +1,13 @@
-package com.github.demo;
+package com.sprng.sprng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SprngApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("hello world");
+		SpringApplication.run(SprngApplication.class, args);
 	}
 
 }
